7a96e3d21d2cb2bca1dfdb15b7c43eb07ed8e563
amelie <amelie.rosso@gmail.com>
2024-01-06 16:28:13 -0800

    implemented google api

--- Test Java/GoogleTranslate.java
+++ Test Java/GoogleTranslate.java
@@ -5,25 +5,32 @@ import java.net.http.HttpResponse;
 import java.net.URLEncoder;
 import org.json.*;
 
-public class Translate {
+public class GoogleTranslate {
+
 	private String keyCode;
-	public Translate(String keyCode) {
+
+	public GoogleTranslate(String keyCode) {
 		this.keyCode = keyCode;
 	}
 
 	public String translate(String sentence, String language) throws Exception {
 		HttpRequest request = HttpRequest.newBuilder()
-		.uri(URI.create("https://google-translate1.p.rapidapi.com/language/translate/v2"))
+		.uri(URI.create("https://translation.googleapis.com/language/translate/v2"))
 		.header("content-type", "application/x-www-form-urlencoded")
 		.header("Accept-Encoding", "application/gzip")
-		.header("X-RapidAPI-Key", this.keyCode)
-		.header("X-RapidAPI-Host", "google-translate1.p.rapidapi.com")
+		.header("X-goog-api-key", this.keyCode)
 		.method("POST", HttpRequest.BodyPublishers.ofString("q=" + URLEncoder.encode(sentence,"UTF-8") + "&target=" + language))
 		.build();
         HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
         String body = response.body();
-		System.out.print(body);
+		//System.out.print(body);
 		JSONObject res = new JSONObject(body);
-		return res.getJSONObject("data").getJSONArray("translations").getJSONObject(0).getString("translatedText");
+        JSONObject data = res.optJSONObject("data");
+        if (data != null) {
+		    return res.getJSONObject("data").getJSONArray("translations").getJSONObject(0).getString("translatedText");
+        }
+        else {
+		    return body;
+        }
 	}
 }
--- Test Java/Main.java
+++ Test Java/Main.java
@@ -1,7 +1,17 @@
+import java.util.Properties;
+import java.io.FileReader;
+
 public class Main {
-    static String sentence = "English is hard but detectably so";
+    static String sentence = "Hello World!";
     public static void main(String[] args) throws Exception {
-        Translate t = new Translate("5101febe8fmsh8b54e70c5bb9063p14f603jsnbb16dbf0213b");
-        t.translate(sentence);
+        Properties p = new Properties();
+        FileReader reader = new FileReader("translate.properties");
+        p.load(reader);
+        //Translate t = new Translate(p.getProperty("rapidapi.apikey"));
+        GoogleTranslate t = new GoogleTranslate(p.getProperty("google.apikey"));
+        System.out.println(t.translate(sentence, "de"));
+        //System.out.println(t.translate(sentence, "fr"));
+        //System.out.println(t.translate(sentence, "es"));
+        //System.out.println(t.translate(sentence, "it"));
     }
 }
